667fbe1f311974ca68e8335842ddeb64754022f7
Arif Laksonodhewo <arifldhewo234@gmail.com>
2025-02-15 23:37:16 +0700

    [feat] create exe file for custom search on kuramanime

---
+++ go/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type Search struct {
+    SearchTitle string `json:"searchTitle"`
+}
+
+func main() {
+    
+	var inputString string;
+    
+    path, err := os.Getwd();
+
+    if err != nil {
+        fmt.Println("Error on Get Current directory: ", err);
+        panic(err);
+    }
+
+    file, err := os.Open(path+"/data/search.json");
+
+    if err != nil {
+        fmt.Println("Error on Opening File: ", err);
+        panic(err);
+    }
+
+    byteValue, err := io.ReadAll(file);
+
+    if err != nil {
+        fmt.Println("Error on Reading JSON File: ", err);
+        panic(err);
+    }
+
+    var search Search;
+
+    err = json.Unmarshal(byteValue, &search);
+
+    reader := bufio.NewReader(os.Stdin);
+
+	fmt.Print("Search anime that you want scrape on kuramanime: \n");
+
+    inputString, _ = reader.ReadString('\n');
+
+    fixedString := strings.TrimSpace(inputString);
+
+    search.SearchTitle = fixedString;
+
+    newJSON, err := json.MarshalIndent(search,"", "    ");
+
+    if err != nil {
+        fmt.Println("Error on Converting to Byte: ", err);
+        panic(err);
+    }
+
+    err = os.WriteFile(path+"/data/search.json", newJSON, 0644);
+
+    if err != nil {
+        fmt.Println("Error on Write File: ", err);
+        panic(err);
+    }
+
+    cmd := exec.Command("cmd", "/C", "kuramanime_search.bat");
+
+    output, err := cmd.CombinedOutput();
+
+    if err != nil {
+        fmt.Println("Error on running CMD: ", err);
+        panic(err);
+    }
+
+    fmt.Println(string(output));
+}
