d2f5cf594c1a67cc759c76c50771bd544c4f1ff1
Arif Laksonodhewo <arifldhewo234@gmail.com>
2025-08-03 17:21:04 +0700

    [task]: update package dependencies

--- KotlinGUI/composeApp/build.gradle.kts
+++ KotlinGUI/composeApp/build.gradle.kts
@@ -20,6 +20,9 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kotlinx.datetime)
+            implementation(libs.ktor.client.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -27,6 +30,7 @@ kotlin {
         jvmMain.dependencies {
             implementation(compose.desktop.currentOs)
             implementation(libs.kotlinx.coroutinesSwing)
+            implementation(libs.ktor.client.cio)
         }
     }
 }
