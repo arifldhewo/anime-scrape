b5a9a0b739e2de576919638d5d82a5fbf6caaf67
Arif Laksonodhewo <arifldhewo234@gmail.com>
2026-01-11 22:20:14 +0700

    [fix] update baseURL that broken

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-	KURAMANIME_BASE_URL: process.env.KURAMANIME_BASE_URL || "https://v9.kuramanime.tel",
+	KURAMANIME_BASE_URL: process.env.KURAMANIME_BASE_URL || "https://v10.kuramanime.tel",
 	SELECTED_DAY: parseInt(process.env.DAY) || undefined,
 	ANIME_FILE_TEMP: process.env.ANIME_FILE_TEMP || "example.json",
 };
