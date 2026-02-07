39a53a772c8c76929b6024b5660f7ea32bccb74d
Arif Laksonodhewo <arifldhewo234@gmail.com>
2026-02-07 17:31:07 +0700

    [fix] resolve dead baseURL

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-	KURAMANIME_BASE_URL: process.env.KURAMANIME_BASE_URL || "https://v11.kuramanime.tel",
+	KURAMANIME_BASE_URL: process.env.KURAMANIME_BASE_URL || "https://v12.kuramanime.tel",
 	SELECTED_DAY: parseInt(process.env.DAY) || undefined,
 	ANIME_FILE_TEMP: process.env.ANIME_FILE_TEMP || "example.json",
 };
