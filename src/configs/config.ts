3ea8a7a7676d508604af09e751ab99022e9ece05
Arif Laksonodhewo <arif.laksonodhewo@dashelectric.co>
2026-06-18 07:36:09 +0700

    [Fix] Patch BASE_URL

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-	KURAMANIME_BASE_URL: process.env.KURAMANIME_BASE_URL || "https://v17.kuramanime.ink",
-	SELECTED_DAY: parseInt(process.env.DAY) || undefined,
+	KURAMANIME_BASE_URL: process.env.KURAMANIME_BASE_URL || "https://v18.kuramanime.ing",
+	SELECTED_DAY: Number(process.env.DAY) || undefined,
 	ANIME_FILE_TEMP: process.env.ANIME_FILE_TEMP || "example.json",
 	MODE: process.env.MODE || "season", // daily, season, search
 };
