f16c2709af9bd3cfab2e989e0da3ed8cb328f94b
Arif Laksonodhewo <arifldhewo.laksonodhewo@dashelectric.co>
2026-09-13 13:33:41 +0700

    [Fix]: Update new url

--- goTUI/extension/kuramanime.go
+++ goTUI/extension/kuramanime.go
@@ -35,7 +35,7 @@ type ResKuramanimeScheduleData struct {
 }
 
 func (k Kuramanime) AnimeDaily() ([]AnimeDaily, error) {
-	baseURL := "https://v17.kuramanime.ink"
+	baseURL := "https://v20.kuramanime.ing"
 
 	res, err := http.Get(fmt.Sprintf("%s/schedule?scheduled_day=%s&need_json=true", baseURL, strings.ToLower(time.Now().Weekday().String())))
 	if err != nil {
--- src/configs/config.ts
+++ src/configs/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-	KURAMANIME_BASE_URL: process.env.KURAMANIME_BASE_URL || "https://v18.kuramanime.ing",
+	KURAMANIME_BASE_URL: process.env.KURAMANIME_BASE_URL || "https://v20.kuramanime.ing",
 	SELECTED_DAY: Number(process.env.DAY) || undefined,
 	ANIME_FILE_TEMP: process.env.ANIME_FILE_TEMP || "example.json",
 	MODE: process.env.MODE || "season", // daily, season, search
